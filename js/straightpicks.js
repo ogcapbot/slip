4c4e7fd3b011500d921494b58d6d002d19753e80
ogcapbot <ogcapbot@gmail.com>
2025-06-26 00:19:14 -0400

    Update straightpicks.js

--- js/straightpicks.js
+++ js/straightpicks.js
@@ -1,10 +1,7 @@
-// js/straightpicks.js
 (() => {
-  // This module handles the straight pick flow (single pick)
-  // Triggered after wager and unit selection, leads to note or hype screen
-
   document.addEventListener("unitSelectionCompleted", () => {
-    // Straight pick flow, go directly to notes
-    document.dispatchEvent(new CustomEvent("notesScreenStart"));
+    if (window.AppState.pickType === "straight") {
+      document.dispatchEvent(new CustomEvent("notesScreenStart"));
+    }
   });
 })();
