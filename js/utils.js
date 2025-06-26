2ef7a3e81440e40df889676242c0552ad124c974
ogcapbot <ogcapbot@gmail.com>
2025-06-26 00:22:33 -0400

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,16 +1,5 @@
-// js/utils.js
 (() => {
-  // Utility functions such as reset button, clearing inputs, etc.
-  // You can add reset button logic here if needed.
-
   window.resetApp = function() {
-    // Reset all state and UI to initial
     window.location.reload();
   };
-
-  // Example reset button creation if you want it
-  // const resetBtn = document.createElement("button");
-  // resetBtn.textContent = "Reset";
-  // resetBtn.onclick = window.resetApp;
-  // document.body.appendChild(resetBtn);
 })();
