2bbfc48771023193672464af690a1829389e0ea1
ogcapbot <ogcapbot@gmail.com>
2025-06-26 08:39:24 -0400

    Delete js/straightpicks.js

--- js/straightpicks.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  document.addEventListener("unitSelectionCompleted", () => {
-    if (window.AppState.pickType === "straight") {
-      document.dispatchEvent(new CustomEvent("notesScreenStart"));
-    }
-  });
-})();
