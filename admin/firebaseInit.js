aa3a78485f1d806991c9529e8e6722d3444ec5c7
ogcapbot <ogcapbot@gmail.com>
2025-06-28 14:48:25 -0400

    Update firebaseInit.js

--- admin/firebaseInit.js
+++ admin/firebaseInit.js
@@ -1,6 +1,6 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
+import { getFirestore, collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB2ggF-0vtAyLhoftOIVnFbzfSpYYzy6rw",
