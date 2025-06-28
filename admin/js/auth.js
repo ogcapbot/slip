ba7cc2e33fdd16447ac297d96dda4d8b1b7b4442
ogcapbot <ogcapbot@gmail.com>
2025-06-28 14:03:39 -0400

    Update auth.js

--- admin/js/auth.js
+++ admin/js/auth.js
@@ -1,5 +1,5 @@
 // login.js
-import { db } from './admin/firebaseInit.js';
+import { db } from '/admin/firebaseInit.js';
 import { collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
 
 const loginSection = document.getElementById('loginSection');
