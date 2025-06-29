830f33c52ff7f9a53996d0be953a38a8d62171a1
ogcapbot <ogcapbot@gmail.com>
2025-06-28 22:31:40 -0400

    Update firebaseInit.js

--- admin/firebaseInit.js
+++ admin/firebaseInit.js
@@ -1,6 +1,7 @@
 // firebaseInit.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
+import { getAuth, RecaptchaVerifier } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB2ggF-0vtAyLhoftOIVnFbzfSpYYzy6rw",
@@ -18,4 +19,19 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firestore DB
 const db = getFirestore(app);
 
-export { db };
+// Initialize Firebase Auth and reCAPTCHA verifier
+const auth = getAuth(app);
+let recaptchaVerifier;
+
+function initRecaptcha() {
+  if (!recaptchaVerifier) {
+    recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
+      'size': 'invisible',
+      'callback': (response) => {
+        console.log('reCAPTCHA solved');
+      }
+    }, auth);
+  }
+}
+
+export { db, auth, recaptchaVerifier, initRecaptcha };
