9dff9922a1785b7581faa8b3a40d699db918e989
ogcapbot <ogcapbot@gmail.com>
2025-07-03 14:07:05 -0400

    Update firebaseInit.js

--- admin/firebaseInit.js
+++ admin/firebaseInit.js
@@ -14,8 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase app
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore DB
 const db = getFirestore(app);
 
-export { db };
+export { app, db };
