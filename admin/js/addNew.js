9e72d83ffee7626bd15a7e7535244ef883425541
ogcapbot <ogcapbot@gmail.com>
2025-07-04 21:05:27 -0400

    Update addNew.js

--- admin/js/addNew.js
+++ admin/js/addNew.js
@@ -12,7 +12,7 @@ import {
   addDoc,
 } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js';
 
-const PAGE_LIMIT = 15;
+const PAGE_LIMIT = 36;
 
 export class AddNewWorkflow {
   constructor(container, userId) {
